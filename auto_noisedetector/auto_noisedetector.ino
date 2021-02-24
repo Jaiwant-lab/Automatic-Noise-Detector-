53f3efd209ea0f7a7c7870ec03c19d23169c4886
Jerry Zhang <zhangjerry931@gmail.com>
2021-02-24 14:25:23 -0800

    this is the code for the arduino

---
+++ auto_noisedetector/auto_noisedetector.ino
@@ -0,0 +1,19 @@
+const int SOUND_PIN = A1;
+const int LED_PIN = 13;
+void setup() {
+  pinMode(SOUND_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int value = analogRead(SOUND_PIN);
+  Serial.println(value);
+  if(value > 500){
+    digitalWrite(LED_PIN, HIGH);
+    delay(1000);
+  }else{
+    digitalWrite(LED_PIN, LOW);
+    delay(1000);
+  }
+}
